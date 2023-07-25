7692ba74895a2f10e13fc2b1b1311c68921f082d
mshchukin <mshchukin@playtika.com>
2023-07-25 16:27:40 +0300

    homework 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import {useState} from "react";
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [data, setData] = useState({name: 'Stepan', age: 25, textHidden: false, buttonHidden: false})
+
+  const changeNameAndAge = () => {
+    setData((prevState) => ({
+      ...prevState,
+      name: 'Mykola',
+      age: 30
+    }))
+  }
+
+  const changeTextVisibility = () => {
+    setData((prevState) => ({
+      ...prevState,
+      textHidden: !prevState.textHidden,
+
+    }))
+  }
+
+  const changeButtonVisibility = () => {
+    setData((prevState) => ({
+      ...prevState,
+      buttonHidden: !prevState.buttonHidden,
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        {data.textHidden ? null :
+            <header>
+              <p>{data.name} age is {data.age} years</p>
+            </header>
+        }
+        <button onClick={changeNameAndAge}>Change name and age</button>
+        <button onClick={changeTextVisibility}>Change text visibility</button>
+        <button onClick={changeButtonVisibility}>{data.buttonHidden ? 'Hidden' : 'Visible'}</button>
+      </div>
   );
 }
 
